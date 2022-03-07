e4507612635bd9910e2e06b25b9fb3fc7835250f
James Forrest <james@forrestclan.com>
2022-03-07 16:32:43 +1100

    Added basic sprite map

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use bevy::prelude::*;
+/*
+fn hello_world() {
     println!("Hello, world!");
+}
+*/
+
+fn main () {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        //.add_system(hello_world)
+        .run()
 }
