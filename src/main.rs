a419f63c6d78863190293c6a12938b9d214e79c9
James Forrest <james@forrestclan.com>
2022-03-07 16:51:16 +1100

    loads and displays the tile map

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,18 @@ fn hello_world() {
 }
 */
 
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("tile-map.png"),
+        ..Default::default()
+    });
+}
+
 fn main () {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
         //.add_system(hello_world)
         .run()
 }
